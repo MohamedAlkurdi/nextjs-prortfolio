de16d8a7e36fef71a146b798fd501f82d1fa041e
MohamedAlkurdi <muhammed.alkurdi.he@gmail.com>
2025-03-04 02:42:22 +0300

    gave up using next-intl because of the unsolvable errors, going to apply a custom internationalization logic

--- src/app/[locale]/blog/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function page({ params }) {
-    console.log(params.locale)
-    console.log(params.id)
-    return (
-        <div>
-            <h>Dynamic blog page</h>
-        </div>
-    )
-}
